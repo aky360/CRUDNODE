4f920d2c111223cb7e4ad2f6d97f5d6bf96d35e3
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-13 18:31:04 +0530

    Add files via upload

---
+++ MERN project/backend/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+     mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
+          console.log(`the database is connected to host ${data.connection.host}`, process.env.DB_URI)
+     }).catch((err) => {
+          console.log('error', err);
+     });
+};
+
+module.exports = connectDatabase;
