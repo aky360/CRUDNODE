da9e73c9ff1c491a271715ccdaf986fa83e09ff8
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-16 00:13:20 +0530

    Update productController.js

--- MERN project/backend/controllers/productController.js
+++ MERN project/backend/controllers/productController.js
@@ -1,3 +1,64 @@
-exports.getAllProducts = (req, res) => {
-     res.status(200).json({ message: "route is working fine" })
+const Product = require("../models/productModel");
+
+
+
+exports.createProduct = async (req, res, next)=>{
+     const product = await Product.create(req.body);
+
+     res.status(200).json({
+          success : true,
+          product
+     })
+}
+
+exports.getAllProducts = async (req, res, next) => {
+     const products = await Product.find({}); 
+     res.status(200).json({
+          success : true,
+          products
+     })
 };
+
+
+exports.updateProduct = async(req, res, next)=>{
+     let product = Product.findById(req.params.id);
+
+     console.log(product);
+
+     if(!product){
+          return res.status(200).json({
+               success :true,
+               message:"Product not found"
+          })
+     }
+     product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+          new:true,
+          runValidators :true,
+          useFindAndModify:false
+     });
+
+     res.status(200).json({
+          success : true,
+          product
+     })
+};
+
+
+
+exports.deletaProduct = async(req, res, next)=>{
+     const product = await Product.findById(req.params.id);
+     console.log("this product to be deleted",product);
+     if(!product){
+          return res.status(200).json({
+               success :true,
+               message:"Product not found"
+          })
+     }
+
+     await product.remove();
+     res.status(200).json({
+          success:true,
+          message:"product deleted successfully"
+     })
+
+}
