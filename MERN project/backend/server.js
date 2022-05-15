143e7ad90d8bc8de721f43ac669e818cdeb85243
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-16 00:17:53 +0530

    Update server.js

--- MERN project/backend/server.js
+++ MERN project/backend/server.js
@@ -1,11 +1,5 @@
 const app = require("./app");
-// app.set('PORT', process.env.PORT || 4000); app.get('PORT');
-const dotenv = require('dotenv');
-dotenv.config({ path: 'backend/config/config.env' });
-//   OR YOU CAN USE IN SINGLE LINE AS BELOW
-//require("dotenv").config({path:"config/config.env"});
-const connectDatabase = require('./config/database');
-connectDatabase();
+
 
 app.listen(process.env.PORT, () => {
      console.log(`the server is running on port is  ${process.env.PORT}`);
