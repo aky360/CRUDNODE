037603046f16a7ed1b3aec5f62c1b23d5d51f6db
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-16 00:17:19 +0530

    Update app.js

--- MERN project/backend/app.js
+++ MERN project/backend/app.js
@@ -4,6 +4,18 @@ const bodyParser = require('body-parser');
 
 app.use(express.json());
 
+
+
+// app.set('PORT', process.env.PORT || 4000); app.get('PORT');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'backend/config/config.env' });
+//   OR YOU CAN USE IN SINGLE LINE AS BELOW
+//require("dotenv").config({path:"config/config.env"});
+const connectDatabase = require('./config/database');
+connectDatabase();
+
+
+
 const product = require('./routes/productRoute');
 
 app.use("/api/v1", product);
