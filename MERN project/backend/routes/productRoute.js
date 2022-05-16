5837031e191d008abbabc22b68fa70563b1b35d8
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-16 12:48:04 +0530

    Update productRoute.js

--- MERN project/backend/routes/productRoute.js
+++ MERN project/backend/routes/productRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deletaProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deletaProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
 
 router.route("/product/new").post(createProduct);
-
 
 router.route("/product/:id").put(updateProduct).delete(deletaProduct).get(getProductDetails);
 
