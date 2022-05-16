f161b8cdcdfd5ca28e9cfccf1a441a7c2b87cdc2
aky360 <95171943+aky360@users.noreply.github.com>
2022-05-16 12:46:04 +0530

    Update server.js

--- MERN project/backend/server.js
+++ MERN project/backend/server.js
@@ -1,6 +1,16 @@
 const app = require("./app");
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
      console.log(`the server is running on port is  ${process.env.PORT}`);
 });
+
+
+process.on("unhandledRejection", (err) => {
+     console.log(`Error: ${err.message}`);
+     console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+     server.close(() => {
+          process.exit(1);
+     });
+})
